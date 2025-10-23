1cc9ef4e1f820ad89b551d553a5d3937e770eda4
Michael Kanios <mkanios77@gmail.com>
2025-10-23 12:04:28 +0000

    Navbar

--- ChronoPhos/app/components/Navbar.tsx
+++ ChronoPhos/app/components/Navbar.tsx
@@ -1,3 +1,7 @@
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { FaBandcamp } from "react-icons/fa";
 const Navbar = () => {
   return (
     <div className="flex justify-between items-center py-4 w-full px-8 fixed top-0 left-0 right-0 z-50 bg-transparent">
@@ -48,18 +52,21 @@ const Navbar = () => {
         className="hidden md:flex xl:space-x-4 md:space-x-2"
       >
         <a
-          href="https://spotify.com"
+          href="https://www.youtube.com/@ChronoPhos"
           target="_blank"
           className="transform transition-transform duration-300 hover:scale-125"
         >
-          <img
-            src="app/assets/images/spotify.png"
-            alt="Chrono Phos Spotify"
-            className="xl:w-8 xl:h-8 md:w-6 md:h-6"
-          />
+          <FaYoutube className="xl:w-8 xl:h-8 md:w-6 md:h-6  text-red-700 hover:text-red-500 transition-colors duration-300" />
         </a>
         <a
-          href="https://chronoPhos.bandcamp.com/album/no-time"
+          href="https://chronophos.bandcamp.com/music"
+          target="_blank"
+          className="transform transition-transform duration-300 hover:scale-125"
+        >
+          <FaBandcamp className="xl:w-8 xl:h-8 md:w-6 md:h-6 rounded-full text-indigo-500 hover:text-indigo-300 transition-colors duration-300 " />
+        </a>
+        <a
+          href="https://chronophos.bandcamp.com/music"
           target="_blank"
           className="transform transition-transform duration-300 hover:scale-125"
         >
@@ -70,37 +77,30 @@ const Navbar = () => {
           />
         </a>
         <a
-          href="https://youtube.com"
+          href="https://spotify.com"
           target="_blank"
           className="transform transition-transform duration-300 hover:scale-125"
         >
           <img
-            src="app/assets/images/youtube.png"
-            alt="Chrono Phos YouTube"
+            src="app/assets/images/spotify.png"
+            alt="Chrono Phos Spotify"
             className="xl:w-8 xl:h-8 md:w-6 md:h-6"
           />
         </a>
+
         <a
           href="https://www.facebook.com/profile.php?id=61567174657178"
           target="_blank"
           className="transform transition-transform duration-300 hover:scale-125"
         >
-          <img
-            src="app/assets/images/facebook.png"
-            alt="Chrono Phos Facebook"
-            className="xl:w-8 xl:h-8 md:w-6 md:h-6"
-          />
+          <FaFacebook className="xl:w-8 xl:h-8 md:w-6 md:h-6 text-blue-500 hover:text-blue-300 transition-colors duration-300" />
         </a>
         <a
-          href="https://www.facebook.com/profile.php?id=61567174657178"
+          href="https://www.instagram.com/chronophos_mk/"
           target="_blank"
           className="transform transition-transform duration-300 hover:scale-125"
         >
-          <img
-            src="app/assets/images/facebook.png"
-            alt="Chrono Phos Facebook"
-            className="xl:w-8 xl:h-8 md:w-6 md:h-6"
-          />
+          <FaInstagram className="xl:w-8 xl:h-8 md:w-6 md:h-6 text-pink-600 hover:text-pink-400 transition-colors duration-300" />
         </a>
       </div>
     </div>
