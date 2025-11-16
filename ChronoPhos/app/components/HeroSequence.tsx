ebac12fbae27e020af54e5780783f51cbd93a6ef
Michael Kanios <mkanios77@gmail.com>
2025-11-16 19:58:45 +0000

    Testing

--- ChronoPhos/app/components/HeroSequence.tsx
+++ ChronoPhos/app/components/HeroSequence.tsx
@@ -62,7 +62,7 @@ export default function HeroSequence() {
           animate={{ opacity: 1, y: 0 }}
           transition={{ delay: videos.length * 1.3 }}
         >
-          🎵Listen Now
+          Listen Now
         </motion.p>
 
         <motion.div
