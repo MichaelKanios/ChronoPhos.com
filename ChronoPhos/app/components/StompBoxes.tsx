ab9ea8a71b553c7363e88c0e86a8bbd2ab527354
Michael Kanios <mkanios77@gmail.com>
2025-11-20 11:27:46 +0000

    Boxes

--- ChronoPhos/app/components/StompBoxes.tsx
+++ ChronoPhos/app/components/StompBoxes.tsx
@@ -1,7 +1,7 @@
 const StompBoxes = () => {
 return (
     <div className="mt-12 ">
-         <h1 className="text-3xl font-semibold text-center mx-auto">Stomboxes</h1>
+         <h1 className="text-3xl font-semibold text-center mx-auto">Chrono-boxes</h1>
 
             <p className="text-sm text-slate-500 text-center mt-2 max-w-lg mx-auto">Here’s where all the magic happens — the heart of my sound and the place where everything gets mixed and transformed.</p>
 
