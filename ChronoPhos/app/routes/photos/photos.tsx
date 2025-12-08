bce0ec0a81986ce4dbb2ebbd4d57f6ad19232990
Michael Kanios <mkanios77@gmail.com>
2025-12-08 13:29:55 +0000

    Fix

--- ChronoPhos/app/routes/photos/photos.tsx
+++ ChronoPhos/app/routes/photos/photos.tsx
@@ -1,10 +1,12 @@
-import ComingSoon from "../../components/ComingSoon";
+import Gallery from "../../components/Gallery";
+
 import { Link } from "react-router-dom";
 
 const Photos = () => {
   return (
     <div className="text-center">
-      <ComingSoon />
+      <Gallery />
+
       <Link
         className="font-chrono text-white text-center text-xl md:text-2xl 
     tracking-wide rounded  hover:bg-yellow-600  px-4 py-2 
